c096ee8137a0568f14068e096742a202584741f3
Raey-Abera <ab.raey@gmail.com>
2021-02-19 21:30:23 -0500

    adding reverse method on linked list

--- linkedLists/linkedList.js
+++ linkedLists/linkedList.js
@@ -38,4 +38,28 @@ class LinkedList {
     getFirst() {
         return this.head
     }
+    //
+    reverse(){
+        // swap the head and tail
+        // create a variable called node and initialize it to the head property
+        let node = this.head
+         this.tail = node
+         this.head = this.tail
+        // create a variable called next
+         let next
+        // create a variable called prev
+        let prev = null
+        // loop through list
+        for(let i = 0; i < this.length; i++){
+          // set next to be the next property on whatever node is
+          next = node.next
+          // set the next property on the node to be whatever prev is
+          node.next = prev
+          // set prev to be the value on the node variable
+          perv = node
+           // set the node variable to be the value of the next variable
+           node = next
+        }
+        return this
+      }
 }
